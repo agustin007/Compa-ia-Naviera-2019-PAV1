83515806631fbfb6d85a6c9cf104b7b4f7479704
agustin007 <agustincarranza54@gmail.com>
2019-09-03 15:28:05 -0300

    Subo base de datos y corrijo proyecto

---
+++ Naviera/Compania_naviera/Presentacion/consulta_navios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Compania_naviera.Presentacion
+{
+    public partial class consulta_navios : Form
+    {
+        public consulta_navios()
+        {
+            InitializeComponent();
+        }
+    }
+}
