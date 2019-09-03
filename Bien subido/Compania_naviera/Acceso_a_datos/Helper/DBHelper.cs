6b0ed6691c8f8ef5b23afe6ac49d3f1e7db29842
agustin007 <agustincarranza54@gmail.com>
2019-09-03 15:16:54 -0300

    De vuelta

---
+++ Bien subido/Compania_naviera/Acceso_a_datos/Helper/DBHelper.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Compania_naviera.Acceso_a_datos.Helper
+{
+    public class DBHelper
+    {
+        private string cadena_conexion;
+        private static DBHelper instancia = new DBHelper();
+
+        private DBHelper()
+        {
+            cadena_conexion = "Data Source=DESKTOP-55MGULP;Initial Catalog=Compañia_Naviera;Integrated Security=True";
+        }
+
+        public static DBHelper getDBHelper()
+        {
+            if (instancia == null)
+                instancia = new DBHelper();
+            return instancia;
+        }
+
+
+        // Resumen:
+        //     Se utiliza para sentencias SQL del tipo “Insert/Update/Delete”. Recibe por valor una sentencia sql como string
+        // Devuelve:
+        //      un valor entero con el número de filas afectadas por la sentencia ejecutada
+        // Excepciones:
+        //      System.Data.SqlClient.SqlException:
+        //          El error de conexión se produce:
+        //              a) durante la apertura de la conexión
+        //              b) durante la ejecución del comando.
+
+        public int ejecutarSQL(string strSql)
+        {
+
+            SqlConnection cnn = new SqlConnection();
+            SqlCommand cmd = new SqlCommand();
+
+            try
+            {
+                cnn.ConnectionString = cadena_conexion;
+                cnn.Open();
+
+                cmd.Connection = cnn;
+                cmd.CommandText = strSql;
+                return cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                this.CloseConnection(cnn);
+            }
+        }
+
+        // Resumen:
+        //     Se utiliza para sentencias SQL del tipo “Select”. Recibe por valor una sentencia sql como string
+        // Devuelve:
+        //      un objeto de tipo DataTable con el resultado de la consulta
+        // Excepciones:
+        //      System.Data.SqlClient.SqlException:
+        //          El error de conexión se produce:
+        //              a) durante la apertura de la conexión
+        //              b) durante la ejecución del comando.
+
+        public DataTable ConsultaSQL(string strSql)
+        {
+            SqlConnection cnn = new SqlConnection();
+            SqlCommand cmd = new SqlCommand();
+            DataTable tabla = new DataTable();
+
+            try
+            {
+                cnn.ConnectionString = cadena_conexion;
+                cnn.Open();
+                cmd.Connection = cnn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = strSql;
+                tabla.Load(cmd.ExecuteReader());
+                return tabla;
+            }
+            catch (SqlException ex)
+            {
+                throw (ex);
+            }
+            finally
+            {
+                this.CloseConnection(cnn);
+            }
+        }
+
+        // Resumen:
+        //      Se utiliza para sentencias SQL del tipo “Select” con parámetros recibidos desde la interfaz
+        //      La función recibe por valor una sentencia sql como string y un arreglo de objetos como parámetros
+        // Devuelve:
+        //      un objeto de tipo DataTable con el resultado de la consulta
+        // Excepciones:
+        //      System.Data.SqlClient.SqlException:
+        //          El error de conexión se produce:
+        //              a) durante la apertura de la conexión
+        //              b) durante la ejecución del comando.
+
+        public DataTable ConsultarSQLConParametros(string sqlStr, Object[] prs)
+        {
+            SqlConnection cnn = new SqlConnection();
+            SqlCommand cmd = new SqlCommand();
+            DataTable tabla = new DataTable();
+
+            string n_param;
+            try
+            {
+                cnn.ConnectionString = cadena_conexion;
+                cnn.Open();
+                cmd.Connection = cnn;
+
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = sqlStr;
+
+                //Agregamos a la colección de parámetros del comando los filtros recibidos
+                //IMPORTANTE: cada parametro deberá llamarse: param1, param2,.., paramN
+                for (int i = 0; i < prs.Length; i++)
+                    if (prs[i] != null)
+                    {
+                        n_param = "param" + Convert.ToString(i + 1);
+                        cmd.Parameters.AddWithValue(n_param, prs[i]);
+                    }
+
+                tabla.Load(cmd.ExecuteReader());
+                return tabla;
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+            finally
+            {
+                CloseConnection(cnn);
+            }
+        }
+
+        private void CloseConnection(SqlConnection cnn)
+        {
+            if (cnn.State == ConnectionState.Open)
+            {
+                cnn.Close();
+                cnn.Dispose();
+            }
+
+        }
+    }
+}
---
+++ Bien subido/Compania_naviera/Presentacion/navios.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace Compania_naviera.Presentacion
+{
+    partial class navios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txt_codigo = new System.Windows.Forms.TextBox();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.txt_altura = new System.Windows.Forms.TextBox();
+            this.txt_autonomia = new System.Windows.Forms.TextBox();
+            this.txt_desplazamiento = new System.Windows.Forms.TextBox();
+            this.txt_eslora = new System.Windows.Forms.TextBox();
+            this.txt_manga = new System.Windows.Forms.TextBox();
+            this.txt_cantiada_pasajeros = new System.Windows.Forms.TextBox();
+            this.txt_cantidad_tripulantes = new System.Windows.Forms.TextBox();
+            this.txt_cantidad_motores = new System.Windows.Forms.TextBox();
+            this.cmb_tipo = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(122, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo navio:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nombre: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(154, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Altura: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(134, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Autonomia:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(106, 151);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Desplazamiento: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(152, 176);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Eslora: ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(424, 51);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Manga: ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(317, 75);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(153, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Cantidad maxima de pasajeros:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(311, 99);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(159, 13);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Cantidad maxima de tripulantes: ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(360, 125);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(110, 13);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Cantidad de motores: ";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(366, 149);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(104, 13);
+            this.label11.TabIndex = 1;
+            this.label11.Text = "Tipo de clasificacion";
+            // 
+            // txt_codigo
+            // 
+            this.txt_codigo.Location = new System.Drawing.Point(200, 44);
+            this.txt_codigo.Name = "txt_codigo";
+            this.txt_codigo.Size = new System.Drawing.Size(100, 20);
+            this.txt_codigo.TabIndex = 2;
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(200, 70);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(100, 20);
+            this.txt_nombre.TabIndex = 2;
+            // 
+            // txt_altura
+            // 
+            this.txt_altura.Location = new System.Drawing.Point(200, 96);
+            this.txt_altura.Name = "txt_altura";
+            this.txt_altura.Size = new System.Drawing.Size(100, 20);
+            this.txt_altura.TabIndex = 2;
+            // 
+            // txt_autonomia
+            // 
+            this.txt_autonomia.Location = new System.Drawing.Point(200, 122);
+            this.txt_autonomia.Name = "txt_autonomia";
+            this.txt_autonomia.Size = new System.Drawing.Size(100, 20);
+            this.txt_autonomia.TabIndex = 2;
+            // 
+            // txt_desplazamiento
+            // 
+            this.txt_desplazamiento.Location = new System.Drawing.Point(200, 148);
+            this.txt_desplazamiento.Name = "txt_desplazamiento";
+            this.txt_desplazamiento.Size = new System.Drawing.Size(100, 20);
+            this.txt_desplazamiento.TabIndex = 2;
+            // 
+            // txt_eslora
+            // 
+            this.txt_eslora.Location = new System.Drawing.Point(200, 173);
+            this.txt_eslora.Name = "txt_eslora";
+            this.txt_eslora.Size = new System.Drawing.Size(100, 20);
+            this.txt_eslora.TabIndex = 2;
+            // 
+            // txt_manga
+            // 
+            this.txt_manga.Location = new System.Drawing.Point(476, 48);
+            this.txt_manga.Name = "txt_manga";
+            this.txt_manga.Size = new System.Drawing.Size(115, 20);
+            this.txt_manga.TabIndex = 2;
+            // 
+            // txt_cantiada_pasajeros
+            // 
+            this.txt_cantiada_pasajeros.Location = new System.Drawing.Point(476, 72);
+            this.txt_cantiada_pasajeros.Name = "txt_cantiada_pasajeros";
+            this.txt_cantiada_pasajeros.Size = new System.Drawing.Size(100, 20);
+            this.txt_cantiada_pasajeros.TabIndex = 2;
+            // 
+            // txt_cantidad_tripulantes
+            // 
+            this.txt_cantidad_tripulantes.Location = new System.Drawing.Point(476, 96);
+            this.txt_cantidad_tripulantes.Name = "txt_cantidad_tripulantes";
+            this.txt_cantidad_tripulantes.Size = new System.Drawing.Size(100, 20);
+            this.txt_cantidad_tripulantes.TabIndex = 2;
+            // 
+            // txt_cantidad_motores
+            // 
+            this.txt_cantidad_motores.Location = new System.Drawing.Point(476, 122);
+            this.txt_cantidad_motores.Name = "txt_cantidad_motores";
+            this.txt_cantidad_motores.Size = new System.Drawing.Size(100, 20);
+            this.txt_cantidad_motores.TabIndex = 2;
+            // 
+            // cmb_tipo
+            // 
+            this.cmb_tipo.FormattingEnabled = true;
+            this.cmb_tipo.Location = new System.Drawing.Point(476, 146);
+            this.cmb_tipo.Name = "cmb_tipo";
+            this.cmb_tipo.Size = new System.Drawing.Size(121, 21);
+            this.cmb_tipo.TabIndex = 3;
+            // 
+            // navios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Compania_naviera.Properties.Resources.navio;
+            this.ClientSize = new System.Drawing.Size(729, 364);
+            this.Controls.Add(this.cmb_tipo);
+            this.Controls.Add(this.txt_cantidad_motores);
+            this.Controls.Add(this.txt_cantidad_tripulantes);
+            this.Controls.Add(this.txt_cantiada_pasajeros);
+            this.Controls.Add(this.txt_manga);
+            this.Controls.Add(this.txt_eslora);
+            this.Controls.Add(this.txt_desplazamiento);
+            this.Controls.Add(this.txt_autonomia);
+            this.Controls.Add(this.txt_altura);
+            this.Controls.Add(this.txt_nombre);
+            this.Controls.Add(this.txt_codigo);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "navios";
+            this.Text = "navios";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txt_codigo;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.TextBox txt_altura;
+        private System.Windows.Forms.TextBox txt_autonomia;
+        private System.Windows.Forms.TextBox txt_desplazamiento;
+        private System.Windows.Forms.TextBox txt_eslora;
+        private System.Windows.Forms.TextBox txt_manga;
+        private System.Windows.Forms.TextBox txt_cantiada_pasajeros;
+        private System.Windows.Forms.TextBox txt_cantidad_tripulantes;
+        private System.Windows.Forms.TextBox txt_cantidad_motores;
+        private System.Windows.Forms.ComboBox cmb_tipo;
+    }
+}
