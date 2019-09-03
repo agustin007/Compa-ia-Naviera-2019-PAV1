fc101769977fc831e942750fced647810cf12cda
agustin007 <agustincarranza54@gmail.com>
2019-09-03 15:09:28 -0300

    Subo el proyecto

---
+++ Compania_naviera/Compania_naviera/Presentacion/login.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Compania_naviera.Presentacion
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_usuario = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.btn_ingresar = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Compania_naviera.Properties.Resources.candado;
+            this.pictureBox1.Location = new System.Drawing.Point(38, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(108, 113);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(173, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Usuario: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(166, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.Location = new System.Drawing.Point(228, 38);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(126, 20);
+            this.txt_usuario.TabIndex = 2;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(228, 80);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(126, 20);
+            this.txt_password.TabIndex = 2;
+            // 
+            // btn_ingresar
+            // 
+            this.btn_ingresar.Location = new System.Drawing.Point(176, 121);
+            this.btn_ingresar.Name = "btn_ingresar";
+            this.btn_ingresar.Size = new System.Drawing.Size(84, 33);
+            this.btn_ingresar.TabIndex = 3;
+            this.btn_ingresar.Text = "Ingresar";
+            this.btn_ingresar.UseVisualStyleBackColor = true;
+            this.btn_ingresar.Click += new System.EventHandler(this.Btn_ingresar_Click);
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Location = new System.Drawing.Point(270, 121);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(84, 33);
+            this.btn_salir.TabIndex = 3;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Compania_naviera.Properties.Resources.descarga;
+            this.ClientSize = new System.Drawing.Size(404, 202);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_ingresar);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_usuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_usuario;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Button btn_ingresar;
+        private System.Windows.Forms.Button btn_salir;
+    }
+}
---
+++ Compania_naviera/Compania_naviera/Presentacion/login.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Compania_naviera.Acceso_a_datos.Helper;
+
+namespace Compania_naviera.Presentacion
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_ingresar_Click(object sender, EventArgs e)
+        {
+            if(txt_usuario.Text == "")
+            {
+                MessageBox.Show("Debe ingresar un usuario");
+                return;
+            }
+
+            if (txt_password.Text == "")
+            {
+                MessageBox.Show("Debe ingresar una contraseña");
+                return;
+            }
+
+            if(validarCredenciales(txt_usuario.Text,txt_password.Text))
+            {
+                MessageBox.Show("Login correcto");
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña invalidos");
+                txt_password.Text = "";
+                txt_password.Focus();
+            }
+        }
+
+        public bool validarCredenciales(string pUsuario, string pPassword)
+        {
+            bool usuarioValido = false;
+
+            try
+            {
+                string sql = string.Concat(" SELECT * ",
+                                          " FROM Usuarios",
+                                          " WHERE nombre = '", pUsuario, "'");
+
+                DataTable resultado = DBHelper.getDBHelper().ConsultaSQL(sql);
+
+                if(resultado.Rows.Count >= 1)
+                {
+                    if(resultado.Rows[0]["password"].ToString() == pPassword)
+                    {
+                        usuarioValido = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(string.Concat("Error de base de datos: ", ex.Message), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                throw;
+            }
+            return usuarioValido;
+
+        }
+    }
+}
